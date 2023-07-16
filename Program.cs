a4352b738dd429ac1d71a075dea0a81f60958681
Dron07Stm12 <dronperenen@gmail.com>
2023-07-16 17:30:18 +0300

    метод Use()

--- Program.cs
+++ Program.cs
@@ -11,6 +11,27 @@ namespace Platform2
 {
     public class Program
     {
+        //через делегат
+        public static void Main()
+        {        
+            CreateHostBuilder().Build().Run();
+        }
+        public static IHostBuilder CreateHostBuilder()
+        {
+            //—реда выполнени€ .NET Core вызывает метод Main, который вызывает метод CreateHostBuilder.
+            //ѕервый шаг в настройке Ч это вызов метода Host.CreateDefaultBuilder. Ётот метод отвечает
+            //за настройку основных функций платформы ASP.NET Core, включа€ создание сервисов отвечает
+            //за данные конфигурации и ведение журнала. Ётот метод также настраивает HTTP-сервер,с именем Kestrel,
+            //который используетс€ дл€ получени€ HTTP-запросов и добавл€ет поддержку работы с Internet Information Services (IIS).
+            //–езультат метода CreateDefaultBuilder передаетс€ методу ConfigureWebHostDefaults, который выбирает класс Startap
+            //в качестве следующего шага в процессе запуска.
+            return Host.CreateDefaultBuilder().ConfigureWebHostDefaults(delegate (IWebHostBuilder webHost)
+            {
+               webHost.UseStartup<Startup>();
+            });
+        }
+
+        //через л€мбду
         //public static void Main(string[] args)
         //{
         //    CreateHostBuilder(args).Build().Run();
@@ -23,22 +44,5 @@ namespace Platform2
         //            webBuilder.UseStartup<Startup>();
         //        });
 
-
-
-        public static void Main()
-        {
-            //Creat().Build().Start();
-            Creat().Build().Run();
-
-        }
-
-        public static IHostBuilder Creat() {
-            return Host.CreateDefaultBuilder().ConfigureWebHostDefaults(delegate (IWebHostBuilder webHost) {
-            webHost.UseStartup<Startup>();
-        }); }
-
-
-
-        
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -13,26 +13,59 @@ namespace Platform2
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //используем делегат Func
+            Func<HttpContext, Func<Task>,Task> func = delegate (HttpContext context,Func<Task> task) {
+
+                if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true" )
+                {
+                    Task tsk0 = context.Response.WriteAsync("Custom Middleware\n");
+                    return tsk0;
+                   // return context.Response.WriteAsync("Custom Middleware\n");
+                }
+                Task tsk = task();
+                return tsk;
+
+            };
+            //ложим эту ссылку в метод Use дл€ регистрации компонента промежуточного сло€
+            app.Use(func);
+            //јргументы  Ч это объект HttpContext и функци€, котора€ вызываетс€, чтобы указать ASP.NET Core передать запрос
+            //к следующему компоненту промежуточного программного обеспечени€ в конвейере.
+            app.Use(delegate (HttpContext context, Func<Task> task) {
+
+                if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom2"] == "true")
+                {
+                    return context.Response.WriteAsync("Custom2 Middleware\n");
+                }
+                return task.Invoke();           
+            });
+
+            app.Use(async(context,task) => {
+
+                if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom3"] == "true")
+                {
+                    await context.Response.WriteAsync("Custom3 Middleware\n");
+                }
+                await task();
+            });
+
 
             app.UseRouting();
 
             app.UseEndpoints(delegate (IEndpointRouteBuilder endpoint) {  endpoint.MapGet("/", delegate (HttpContext context) {
                 return context.Response.WriteAsync("Hello Dron"); }); 
             });
-            //dfsdfsdfsasas
+           
         }
     }
 }
